44b6ea55e27f229b5dc50eb3fb9bd90fc9617aef
赵自通 <2718090911@qq.com>
2019-08-08 22:06:09 +0800

    *）完善Map映射

--- ZZT.Mapper/ZZTMapper.Test/UnitTest1.cs
+++ ZZT.Mapper/ZZTMapper.Test/UnitTest1.cs
@@ -1,28 +1,33 @@
 using System;
 using Xunit;
 using ZZT.Mapper;
+using ZZT.Mapper.Attr;
 
-namespace ZZTMapper.Test
+namespace ZZT.Mapper.Test
 {
     public class UnitTest1
     {
         [Fact]
         public void MapTest()
         {
-            Person per = new Person { FirstName = "firstName", LastName = "lastName" };
-            Customer cus = AutoMapper.Map<Customer, Person>(per);
+            Person per = new Person { FirstName = "firstName", LastName = "lastName",Notes="notes" };
+            Customer cus = ZZTMapper.Map<Customer, Person>(per);
           
-            Customer cus2 = new Customer { FirstName = "firstName2", LastName = "lastName2" };
-            Person per2 = AutoMapper.Map<Person, Customer>(cus2);
+            Customer cus2 = new Customer { FirstName = "firstName2", LastName = "lastName2",MyNotes="mynotes" };
+            Person per2 = ZZTMapper.Map<Person, Customer>(cus2);
            
         }
     }
 
+    [AutoMap(typeof(Customer))]
     public class Person
     {
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        [MapToOrFromProperty(typeof(Customer),nameof(Customer.MyNotes))]
+        public string Notes { get; set; }
     }
 
     public class Customer
@@ -30,5 +35,7 @@ namespace ZZTMapper.Test
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public string MyNotes { get; set; }
     }
 }
